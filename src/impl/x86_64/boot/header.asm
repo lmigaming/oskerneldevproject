70136bd51f8e054ff8c367a2254cacc5a29ee03e
LMI <l.molnar.ivan@gmail.com>
2024-02-14 17:07:52 +0100

    added code from first lesson

---
+++ src/impl/x86_64/boot/header.asm
@@ -0,0 +1,15 @@
+section .multiboot_header
+
+header_start:
+    ; magic number
+    dd 0xe85250d6
+    ; architecture info
+    dd 0 ; protected mode
+    dd header_end - header_start
+    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
+
+    ;end tag
+    dw 0
+    dw 0
+    dd 8
+header_end:
